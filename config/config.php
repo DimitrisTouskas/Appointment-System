3556b9c1c8f5e69dbecf25e8d03ca6cbacc91c3b
Dimitris Touskas <dtouskas@hotmail.com>
2026-07-05 04:17:45 +0300

    Redirect app errors to logs/ folder, keep it tracked via local .gitignore

--- config/config.php
+++ config/config.php
@@ -3,6 +3,7 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
 $dotenv->load();
+ini_set('error_log', __DIR__ . '/../logs/app.log');
 
 return [
     'host' => $_ENV['DB_HOST'],
